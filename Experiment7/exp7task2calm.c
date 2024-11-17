a8925f3058d81f1c3e09d7b8917840ee576e503f
Aquib-FK <aquibmansuri11@eng.rizvi.edu.in>
2024-11-18 00:03:48 +0530

    Update exp7task2calm.c

--- Experiment7/exp7task2calm.c
+++ Experiment7/exp7task2calm.c
@@ -1,7 +1,7 @@
 /*Aim : WAP to find the Largest element in an array.
-Name : Raj Maurya
-Roll no : 08
-UIN : 241A008
+Name : Mohammed Aquib 
+Roll no : 06
+UIN : 241A006
 Division : A*/
 #include<stdio.h>
 int main()
